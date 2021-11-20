0b9ab903839a5317998bc3fd769550964cec187c
root <root@dnetapapp01.alfa.bank.int>
2021-11-20 21:05:23 +0200

    iac-9

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,2 +1,7 @@
-variable "AWS_ACCESS_KEY" {AKIAIOSFODNN6OLEX777}
-variable "AWS_SECRET_KEY" {wJalrXUtnFEMI/K7MDENG/bPxRfiOLEX777KEY}
+variable "AWS_ACCESS_KEY" {
+  type   = string
+  default="AKIAIOSFODNN6OLEX777"}
+
+variable "AWS_SECRET_KEY" {
+  type   = string
+  default = "wJalrXUtnFEMI/K7MDENG/bPxRfiOLEX777KEY"}
