0e514e7e4f7393916034a6dc57b5e7ae3a9bed05
root <root@dnetapapp01.alfa.bank.int>
2021-11-20 20:51:28 +0200

    iac-7

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,5 @@ provider "aws" {
   region     = "eu-central-1"
   access_key = "AKIAIOSFODNN6OLEX777"
   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiOLEX777KEY"
-  security_token =  "HzuClwaBFwGmdQ.atlasv1.kvrMLCdU9B2hNuxPqLP2uukx9BYchMiSrO4j5fyRk0wcsnpOIgYzyRz5wyMIewsLb0A"
 }
 
