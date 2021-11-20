93c64f0eaa7df5c198336a5905ca5e4ededc0140
root <root@dnetapapp01.alfa.bank.int>
2021-11-20 23:32:14 +0200

    iac-last

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA5BRF72QHU3IDHHVW"
-  secret_key = "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"
   region     = "eu-central-1"
+  shared_credentials_file = "/root/.aws/credentials"
+  profile                 = "default"
 
 }
