25b49d032732f399bc2928de80959847935e2869
root <root@dnetapapp01.alfa.bank.int>
2021-11-20 20:25:01 +0200

    iac-4

---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = "AKIAIOSFODNN6OLEX777"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiOLEX777KEY"
+}
+
--- terraform/terrafromcloud.tf
+++ terraform/terrafromcloud.tf
@@ -7,9 +7,4 @@ terraform {
     }
   }
 }
-provider "aws" {
-  region     = "eu-central-1"
-  access_key = "AKIAIOSFODNN6OLEX777"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiOLEX777KEY"
-}
 
