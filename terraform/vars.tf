eef3b4630691841ac2ff36802d59ee5048db05fd
root <root@dnetapapp01.alfa.bank.int>
2021-11-21 01:22:31 +0200

    iac-force-8

---
+++ terraform/vars.tf
@@ -0,0 +1,18 @@
+variable "region" {
+    type    = string
+    default = "eu-central-1"
+    sensitive = true
+}
+
+variable "AWS_ACCESS_KEY" {
+    type    = string
+    default = "AKIA5BRF72QHSUGM4ZJB"
+    sensitive = true
+}
+
+variable "AWS_SECRET_KEY" {
+    type    = string
+    default = "5WT8k1EP1buP+rpklLwDqMruWVSu4dQiaoiMYNiO"
+    sensitive = true
+}
+
