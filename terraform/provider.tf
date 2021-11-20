2911d8fe3c5f9ac4de8456e2c351eb830bc11f5f
root <root@dnetapapp01.alfa.bank.int>
2021-11-20 22:01:02 +0200

    iac-16

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  AWS_ACCESS_KEY_ID="AKIA5BRF72QHU3IDHHVW" 
-  AWS_SECRET_ACCESS_KEY="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"
-  AWS_DEFAULT_REGION     = "eu-central-1"
+  access_key = "AKIA5BRF72QHU3IDHHVW"
+  secret_key = "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"
+  region     = "eu-central-1"
 
 }
