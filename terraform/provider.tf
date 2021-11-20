390958ab0b13f9b260dcfcc1ca925002c90dac05
root <root@dnetapapp01.alfa.bank.int>
2021-11-20 20:37:13 +0200

    iac-5

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,5 +2,6 @@ provider "aws" {
   region     = "eu-central-1"
   access_key = "AKIAIOSFODNN6OLEX777"
   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiOLEX777KEY"
+  security_token =  ${{ secrets.TF_API_TOKEN }}
 }
 
